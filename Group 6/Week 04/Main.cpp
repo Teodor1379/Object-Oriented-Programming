4f03ccf4a87d8d3007f8e0236412c360b1195c2b
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2023-05-07 16:33:47 +0300

    Commit: Group 6 - Week 4 - Problem 2

--- Group 6/Week 04/Main.cpp
+++ Group 6/Week 04/Main.cpp
@@ -52,6 +52,123 @@ private:
     char color[64];
 };
 
+class Shisha {
+public:
+    Shisha() {
+        this->price = 0.0;
+        this->brand = nullptr;
+        this->taste = nullptr;
+    }
+
+    Shisha(double price, const char *brand, const char *taste)
+        : price(0.0), brand(nullptr), taste(nullptr) {
+        if (price <= 0.0 || brand == nullptr || taste == nullptr) {
+            std::cout << "Invalid arguments for the shisha!" << std::endl;
+            return;
+        }
+
+        this->brand = new (std::nothrow) char[strlen(brand) + 1];
+
+        if (this->brand == nullptr) {
+            std::cout << "Problem with allocating memory!" << std::endl;
+            return;
+        }
+
+        this->taste = new (std::nothrow) char[strlen(taste) + 1];
+
+        if (this->taste == nullptr) {
+            std::cout << "Problem with allocating memory!" << std::endl;
+            delete[] this->brand; this->brand = nullptr; return;
+        }
+
+        this->price = price;
+        strcpy(this->brand, brand);
+        strcpy(this->taste, taste);
+    }
+
+    double getPrice()   const { return this->price; }
+    char* getBrand()    const { return this->brand; }
+    char* getTaste()    const { return this->taste; }
+
+    void setPrice(double price) {
+        if (price <= 0.0) {
+            std::cout << "Invalid price for the shisha!" << std::endl;
+            return;
+        }
+
+        this->price = price;
+    }
+
+    void setBrand(const char *brand) {
+        if (brand == nullptr) {
+            std::cout << "Invalid brand for the shisha!" << std::endl;
+            return;
+        }
+
+        delete[] this->brand;
+
+        this->brand = new (std::nothrow) char[strlen(brand) + 1];
+
+        if (this->brand == nullptr) {
+            std::cout << "Problem with allocating memory!" << std::endl;
+            this->resetData(); return;
+        }
+
+        strcpy(this->brand, brand);
+    }
+
+    void setTaste(const char *taste) {
+        if (taste == nullptr) {
+            std::cout << "Invalid taste for the shisha!" << std::endl;
+            this->resetData(); return;
+        }
+
+        delete[] this->taste;
+
+        this->taste = new (std::nothrow) char[strlen(taste) + 1];
+
+        if (this->taste == nullptr) {
+            std::cout << "Problem with allocating memory!" << std::endl;
+            return;
+        }
+
+        strcpy(this->taste, taste);
+    }
+
+    void print() const {
+        if (this->brand == nullptr || this->taste == nullptr) {
+            std::cout << "This shisha contains invalid data!" << std::endl;
+            return;
+        }
+
+        std::cout << std::endl;
+        std::cout << "Shisha Information: " << std::endl;
+        std::cout << "Price: " << this->price << std::endl;
+        std::cout << "Brand: " << this->brand << std::endl;
+        std::cout << "Taste: " << this->taste << std::endl;
+        std::cout << std::endl;
+    }
+
+    ~Shisha() {
+        delete[] this->brand;
+        delete[] this->taste;
+    }
+
+private:
+    void resetData() {
+        delete[] this->brand;
+        delete[] this->taste;
+
+        this->price = 0.0;
+        this->brand = nullptr;
+        this->taste = nullptr;
+    }
+
+    double price;
+    char *brand;
+    char *taste;
+};
+
 int main() {
     return 0;
 }
