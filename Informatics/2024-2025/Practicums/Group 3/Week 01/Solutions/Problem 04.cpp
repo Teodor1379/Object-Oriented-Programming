0cd80f5638a0ef2de2f627db148ced4385ba1dea
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2025-04-11 08:30:10 +0300

    Commit: Informatics - 2024-2025 - Practicums - Group 3 - Week 01

--- Informatics/2024-2025/Practicums/Group 3/Week 01/Solutions/Problem 04.cpp
+++ Informatics/2024-2025/Practicums/Group 3/Week 01/Solutions/Problem 04.cpp
@@ -6,58 +6,40 @@
 
 
 
-int             readNumb();
-unsigned int    readSize();
+int     inputSide1();
+double  inputSide2();
 
 
 
-int*    buildArray(                     unsigned int size   );
-void    clearArray(         int*& array                     );
-
-void    inputArray(         int* array, unsigned int size   );
-void    printArray(const    int* array, unsigned int size   );
+int findPerimeter   (int a, int b);
+int findArea        (int a, int b);
 
 
 
-int findMinNumber(const int* array, unsigned int size);
-int findMaxNumber(const int* array, unsigned int size);
+double findPerimeter(double a, double b);
+double findArea     (double a, double b);
 
 
 
-int findArraySum    (const int* array, unsigned int size);
-int findArrayProduct(const int* array, unsigned int size);
-
+void    printData   (int    a,  int     b);
+void    printData   (double a,  double  b);
 
 
 
 int main() {
-    unsigned int size = readSize();
+    int side11 = inputSide1();
+    int side12 = inputSide1();
+
+    printData(side11, side12);
 
 
-    int* array = buildArray(size);
-
-    if (array == nullptr) {
-        std::cout << "Allocating Memory... ERROR!" << std::endl;
-
-        return 1;
-    }
+    std::cout << std::endl;
 
 
-    int minNumber = findMinNumber(array, size);
-    int maxNumber = findMaxNumber(array, size);
-    
-    int result1 = findArraySum      (array, size);
-    int result2 = findArrayProduct  (array, size);
+    double side21 = inputSide2();
+    double side22 = inputSide2();
 
-
-    std::cout << "The MIN number of the array is: " << minNumber << std::endl;
-    std::cout << "The MAX number of the array is: " << maxNumber << std::endl;
-
-    std::cout << "The SUM       of the array is: " << result1 << std::endl;
-    std::cout << "The PRODUCT   of the array is: " << result2 << std::endl;
-
-
-    clearArray(array);
+    printData(side21, side22);
 
 
     return 0;
@@ -65,145 +47,104 @@ int main() {
 
 
 
-int readNumb() {
-    int number = 0;
+int inputSide1() {
+    int side = 0;
 
     while (true) {
-        std::cin >> number;
+        std::cout << "Enter the side of the rectangle: ";
+
+        std::cin >> side;
 
         if (std::cin.fail()) {
             std::cin.clear  ();
             std::cin.ignore (std::numeric_limits<std::streamsize>::max(), '\n');
         } else {
-            return number;
+            if (side <= 0) {
+                continue;
+            } else {
+                return side;
+            }
         }
     }
 }
 
-unsigned int readSize() {
-    unsigned int size = 0;
+double inputSide2() {
+    double side = 0.0;
 
-    while (size == 0) {
-        std::cout << "Enter the size of the array: ";
+    while (true) {
+        std::cout << "Enter the side of the rectangle: ";
 
-        std::cin >> size;
+        std::cin >> side;
 
         if (std::cin.fail()) {
             std::cin.clear  ();
             std::cin.ignore (std::numeric_limits<std::streamsize>::max(), '\n');
-
-            size = 0;
         } else {
-            std::cin.ignore (std::numeric_limits<std::streamsize>::max(), '\n');
-
-            return size;
+            if (side <= 0.0) {
+                continue;
+            } else {
+                return side;
+            }
         }
     }
-
-    return size;
 }
 
 
 
-int* buildArray(unsigned int size) {
-    assert(size !=  0   );
+int findPerimeter(int a, int b) {
+    assert(a > 0);
+    assert(b > 0);
 
-    int* array = new (std::nothrow) int[size];
-
-    if (array != nullptr) {
-        inputArray(array, size);
-        printArray(array, size);
-    }
-    
-    return array;
+    return 2 * (a + b);
 }
 
-void clearArray(int*& array) {
-    assert(array    !=  nullptr );
-    
-    delete[] array;
+int findArea(int a, int b) {
+    assert(a > 0);
+    assert(b > 0);
 
-    array = nullptr;
+    return a * b;
 }
 
 
 
-void inputArray(int* array, unsigned int size) {
-    assert(array    !=  nullptr );
-    assert(size     !=  0       );
+double findPerimeter(double a, double b) {
+    assert(a > 0);
+    assert(b > 0);
 
-    std::cout << "Enter the elements of the array: ";
-
-    for (unsigned int i = 0; i < size; ++i) {
-        array[i] = readNumb();
-    }
+    return 2 * (a + b);
 }
 
-void printArray(const int* array, unsigned int size) {
-    assert(array    !=  nullptr );
-    assert(size     !=  0       );
+double findArea(double a, double b) {
+    assert(a > 0);
+    assert(b > 0);
 
-    std::cout << "The elements of the array are: ";
+    return a * b;
+}
 
-    for (unsigned int i = 0; i < size; ++i) {
-        std::cout << array[i] << ' ';
-    }
+
+
+void printData(int a, int b) {
+    assert(a > 0);
+    assert(b > 0);
+
+    int perimeter   =   findPerimeter   (a, b);
+    int area        =   findArea        (a, b);
 
     std::cout << std::endl;
+
+    std::cout << "The PERIMETER of the rectangle is: " << perimeter << std::endl;
+    std::cout << "The AREA      of the rectangle is: " << area      << std::endl;
 }
 
+void printData(double a, double b) {
+    assert(a > 0);
+    assert(b > 0);
 
+    double perimeter    =   findPerimeter   (a, b);
+    double area         =   findArea        (a, b);
 
-int findMinNumber(const int* array, unsigned int size) {
-    assert(array    !=  nullptr );
-    assert(size     !=  0       );
+    std::cout << std::endl;
 
-    int result = array[0];
-
-    for (unsigned int i = 1; i < size; ++i) {
-        result = std::min(result, array[i]);
-    }
-
-    return result;
-}
-
-int findMaxNumber(const int* array, unsigned int size) {
-    assert(array    !=  nullptr );
-    assert(size     !=  0       );
-
-    int result = array[0];
-
-    for (unsigned int i = 1; i < size; ++i) {
-        result = std::max(result, array[i]);
-    }
-
-    return result;
-}
-
-
-
-int findArraySum(const int* array, unsigned int size) {
-    assert(array    !=  nullptr );
-    assert(size     !=  0       );
-
-    int result = 0;
-
-    for (unsigned int i = 0; i < size; ++i) {
-        result = result + array[i];
-    }
-
-    return result;
-}
-
-int findArrayProduct(const int* array, unsigned int size) {
-    assert(array    !=  nullptr );
-    assert(size     !=  0       );
-
-    int result = 1;
-
-    for (unsigned int i = 0; i < size; ++i) {
-        result = result * array[i];
-    }
-
-    return result;
+    std::cout << "The PERIMETER of the rectangle is: " << perimeter << std::endl;
+    std::cout << "The AREA      of the rectangle is: " << area      << std::endl;
 }
