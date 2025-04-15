586143d4622997f0e5a1fb1db9ce5c855d0cd23b
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2025-04-15 04:04:45 +0300

    Commit: Informatics - 2024-2025 - Practicums - Group 3 - Week 01

--- Informatics/2024-2025/Practicums/Group 3/Week 01/Solutions/Problem 09.cpp
+++ Informatics/2024-2025/Practicums/Group 3/Week 01/Solutions/Problem 09.cpp
@@ -1,53 +1,152 @@
 #include <limits>
 
+#include <cassert>
+
+#include <cstring>
+
 #include <iostream>
 
 
 
-enum Age {
-    YOUNG   ,
-    AVERAGE ,
-    OLD     ,
-    INVALID ,
-};
+#define MAX 1024
 
 
 
-const char* ageStrings[4] = { "Young", "Average", "Old", "Invalid"  };
+unsigned int    readSize();
 
 
 
-int readAge();
+char**  buildStrings(                               unsigned int size);
+void    clearStrings(char**& strings,               unsigned int size);
+void    printStrings(const char* const* strings,    unsigned int size);
+
+
+
+void reverseString(char*& string, std::size_t size);
 
 
 
 int main() {
-    Age age = static_cast<Age>(readAge() / 34);
+    unsigned int size = readSize();
 
-    std::cout << "The interval is: " << ageStrings[age] << std::endl;
-    
+    std::cout << std::endl;
+
+
+    char** strings = buildStrings(size);
+
+    if (strings == nullptr) {
+        std::cout << "Allocating Memory... ERROR!" << std::endl;
+
+        return 1;
+    }
+
+
+    printStrings(strings, size);
+
+
+    for (unsigned int i = 0; i < size; ++i) {
+        reverseString(strings[i], strlen(strings[i]));
+    }
+
+    printStrings(strings, size);
+
+
+    clearStrings(strings, size);
+
+
     return 0;
 }
 
 
 
-int readAge() {
-    int age = 0;
+unsigned int readSize() {
+    unsigned int size = 0;
 
     while (true) {
-        std::cout << "Enter the age of the human: ";
+        std::cout << "Enter the size: ";
 
-        std::cin >> age;
+        std::cin >> size;
 
         if (std::cin.fail()) {
             std::cin.clear  ();
             std::cin.ignore (std::numeric_limits<std::streamsize>::max(), '\n');
         } else {
-            if (age < 1) {
+            if (size == 0) {
                 continue;
-            }
-
-            return age;
+            } else {
+                return size;
+            }             
         }
+    }
+}
+
+
+
+char** buildStrings(unsigned int size) {
+    assert(size !=  0);
+
+    char** strings = new (std::nothrow) char*[size];
+
+    std::cin.ignore();
+
+    std::cout << "Enter the strings: " << std::endl;
+
+    if (strings != nullptr) {
+        for (unsigned int i = 0; i < size; ++i) {
+            strings[i] = new (std::nothrow) char[MAX];
+
+            if (strings[i] == nullptr) {
+                clearStrings(strings, i);
+
+                return nullptr;
+            } else {
+                std::cin.getline(strings[i], MAX, '\n');
+            }
+        }
+    }
+
+    return strings;
+}
+
+void clearStrings(char**& strings, unsigned int size) {
+    assert(strings  !=  nullptr );
+    assert(size     !=  0       );
+
+    for (unsigned int i = 0; i < size; ++i) {
+        delete[] strings[i];
+    }
+
+    delete[] strings;
+
+    strings = nullptr;
+}
+
+void printStrings(const char* const* strings, unsigned int size) {
+    assert(strings  !=  nullptr );
+    assert(size     !=  0       );
+
+    std::cout << std::endl;
+    std::cout << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "The strings are: " << std::endl;
+
+    for (unsigned int i = 0; i < size; ++i) {
+        std::cout << strings[i] << std::endl;
+    }
+
+    std::cout << std::endl;
+    std::cout << std::endl;
+    std::cout << std::endl;
+}
+
+
+
+void reverseString(char*& string, std::size_t size) {
+    assert(string   !=  nullptr );
+    assert(size     !=  0       );
+
+    for (std::size_t i = 0; i < size / 2; ++i) {
+        std::swap(string[i], string[size - i - 1]);
     }
 }
