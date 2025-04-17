d5b83ec7f73da04f1de7d320eb87f9ea37953893
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2025-04-17 15:49:12 +0300

    Commit: Informatics - 2024-2025 - Practicums - Group 3 - Week 01

--- Informatics/2024-2025/Practicums/Group 3/Week 01/Lab/Example 03.cpp
+++ Informatics/2024-2025/Practicums/Group 3/Week 01/Lab/Example 03.cpp
@@ -12,5 +12,8 @@ int main() {
     std::cout << std::boolalpha << "Condition 1: " << condition1 << std::endl;
     std::cout << std::boolalpha << "Condition 2: " << condition2 << std::endl;
 
+    std::cout << std::noboolalpha << "Condition 1: " << condition1 << std::endl;
+    std::cout << std::noboolalpha << "Condition 2: " << condition2 << std::endl;
+
     return 0;
 }
