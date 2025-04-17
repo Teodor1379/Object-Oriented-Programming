768c938f46998430240df531a1c16d0771827df6
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2025-04-17 11:58:04 +0300

    Commit: Informatics - 2024-2025 - Practicums - Group 3 - Week 01

---
+++ Informatics/2024-2025/Practicums/Group 3/Week 01/Solutions/Problem 12.cpp
@@ -0,0 +1,139 @@
+#include <limits>
+
+#include <iostream>
+
+
+
+#define PERMISSIONS 7
+
+
+
+enum class ReadPermissions {
+    EMPTY   =   0,
+    READ    =   1,
+};
+
+enum class WritePermissions {
+    EMPTY   =   0,
+    WRITE   =   1,
+};
+
+enum class ExecutePermissions {
+    EMPTY   =   0,
+    EXECUTE =   1,
+};
+
+
+
+unsigned int    readNumber();
+
+
+
+ReadPermissions     getR(unsigned int number);
+WritePermissions    getW(unsigned int number);
+ExecutePermissions  getE(unsigned int number);
+
+
+
+void printR(ReadPermissions     permissions);
+void printW(WritePermissions    permissions);
+void printE(ExecutePermissions  permissions);
+
+void printPermissions(unsigned int permissions);
+
+
+
+int main() {
+    unsigned int number = readNumber();
+
+    printPermissions(number);
+
+    return 0;
+}
+
+
+
+unsigned int readNumber() {
+    unsigned int number = 0;
+
+    while (true) {
+        std::cout << "Enter the number: ";
+
+        std::cin >> number;
+
+        if (std::cin.fail()) {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        } else {
+            if (number > PERMISSIONS) {
+                continue;
+            }
+
+            return number;
+        }
+    }
+}
+
+
+
+ReadPermissions getR(unsigned int number) {
+    unsigned int bit = number & 1;
+
+    return bit == 0 ? 
+        ReadPermissions::EMPTY      :
+        ReadPermissions::READ       ;
+}
+
+WritePermissions getW(unsigned int number) {
+    unsigned int bit = number & 2;
+
+    return bit == 0 ?
+        WritePermissions::EMPTY     :
+        WritePermissions::WRITE     ;
+}
+
+ExecutePermissions getE(unsigned int number) {
+    unsigned int bit = number & 4;
+
+    return bit == 0 ?
+        ExecutePermissions::EMPTY   :
+        ExecutePermissions::EXECUTE ;
+}
+
+
+
+void printR(ReadPermissions permissions) {
+    if (permissions == ReadPermissions::READ) {
+        std::cout << 'r';
+    } else {
+        std::cout << '-';
+    }
+}
+
+void printW(WritePermissions permissions) {
+    if (permissions == WritePermissions::WRITE) {
+        std::cout << 'w';
+    } else {
+        std::cout << '-';
+    }
+}
+
+void printE(ExecutePermissions permissions) {
+    if (permissions == ExecutePermissions::EXECUTE) {
+        std::cout << 'e';
+    } else {
+        std::cout << '-';
+    }
+}
+
+
+
+void printPermissions(unsigned int permissions) {
+    std::cout << "The permissions are: ";
+
+    printR(getR(permissions));
+    printW(getW(permissions));
+    printE(getE(permissions));
+
+    std::cout << std::endl;
+}
