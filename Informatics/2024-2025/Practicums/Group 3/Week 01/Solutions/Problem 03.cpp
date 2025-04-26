ad3e2f46628f84aaeecffbdcd0ff9b319d3e9449
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2025-04-26 12:45:48 +0300

    Commit: Informatics - 2024-2025 - Practicums - Group 3 - Week 01

--- Informatics/2024-2025/Practicums/Group 3/Week 01/Solutions/Problem 03.cpp
+++ Informatics/2024-2025/Practicums/Group 3/Week 01/Solutions/Problem 03.cpp
@@ -38,10 +38,11 @@ int readNumber() {
         std::cin >> number;
 
         if (std::cin.fail()) {
-            std::cin.clear  ();
-            std::cin.ignore (std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cin.clear  ()                                                  ;
+            std::cin.ignore (std::numeric_limits<std::streamsize>::max(), '\n') ;
         } else {
-            std::cin.ignore (std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cin.clear  ()                                                  ;
+            std::cin.ignore (std::numeric_limits<std::streamsize>::max(), '\n') ;
             
             return number;
         }
