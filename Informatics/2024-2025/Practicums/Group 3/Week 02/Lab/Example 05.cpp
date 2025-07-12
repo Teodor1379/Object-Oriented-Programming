3b9f85b24ca9108aeba7e7b27a3977b72f92ad36
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2025-07-12 11:48:13 +0300

    Commit: Informatics - 2024-2025 - Practicums - Group 3 - Week 02

--- Informatics/2024-2025/Practicums/Group 3/Week 02/Lab/Example 05.cpp
+++ Informatics/2024-2025/Practicums/Group 3/Week 02/Lab/Example 05.cpp
@@ -2,8 +2,53 @@
 
 
 
+#define MAX 256
+
+
+
+struct Person {
+    char name1[MAX];
+    char name2[MAX];
+};
+
+struct RelationShip {
+    Person m;
+    Person f;
+};
+
+
+
+typedef Person          P;
+typedef RelationShip    R;
+
+
+
+void printPersonInformation(const P& person) {
+    std::cout << "Name 1: " << person.name1 << std::endl;
+    std::cout << "Name 2: " << person.name2 << std::endl;
+
+    std::cout << std::endl;
+}
+
+void printRelationShipInformation(const R& relationShip) {
+    std::cout << "M Information:\n";    printPersonInformation(relationShip.m);
+    std::cout << "F Information:\n";    printPersonInformation(relationShip.f);
+}
+
+
+
 int main() {
+    P m = { "John"  , "Wick" };
+    P f = { "Eve"   , "Macc" };
+
+    R r = { m, f };
+
+
     std::cout << "Hello World!" << std::endl;
+
+
+    printRelationShipInformation(r);
+
 
     return 0;
 }
