5ab93a6cde4488f5a79bf28952166c959177d06e
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2025-07-13 14:24:42 +0300

    Commit: Informatics - 2024-2025 - Practicums - Group 3 - Week 04

---
+++ Informatics/2024-2025/Practicums/Group 3/Week 04/Solutions/Problem 02.cpp
@@ -0,0 +1,34 @@
+#include <fstream>
+#include <iostream>
+
+
+
+#define MAX 256
+
+
+
+int main() {
+    char buffer[MAX] = { 0 };
+
+    std::cout << "Enter the file path: ";
+
+    std::cin.getline(buffer, MAX, '\n');
+
+
+    std::ifstream stream(buffer, std::ios::binary);
+
+    if (stream.is_open() == false) {
+        std::cerr << "Error while opening the file!" << std::endl;
+
+        return 1;
+    }
+
+    stream.seekg(0, std::ios_base::end);
+
+    std::cout << "File Size is: " << stream.tellg() << std::endl;
+
+    stream.close();
+
+
+    return 0;
+}
