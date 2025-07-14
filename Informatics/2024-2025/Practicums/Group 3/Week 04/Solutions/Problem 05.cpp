ad213b1b6e26245c1c4dc9fd6c09750ba613e520
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2025-07-15 02:49:13 +0300

    Commit: Informatics - 2024-2025 - Practicums - Group 3 - Week 04

---
+++ Informatics/2024-2025/Practicums/Group 3/Week 04/Solutions/Problem 05.cpp
@@ -0,0 +1,215 @@
+#include <cstdlib>
+
+#include <fstream>
+#include <iostream>
+
+
+
+#define MAX 10
+
+
+
+const char* FILE_PATH = "Numbers.dat";
+
+
+
+const char* ERROR_FILE_R = "Error while reading from    the file!";
+const char* ERROR_FILE_W = "Error while writing to      the file!";
+const char* ERROR_FILE_O = "Error while opening         the file!";
+const char* ERROR_FILE_D = "Error while operating with  the file!";
+
+const char* ERROR_STATUS_E = "EMPTY"    ;
+const char* ERROR_STATUS_C = "CORRUPTED";
+
+
+
+void generator(const int32_t[MAX], unsigned int);
+
+
+void    readFile();
+void    sortFile();
+
+
+
+int main() {
+    const int32_t data1[MAX] = { 1, 2, 3, 0, 0, 0, 0, 0, 0, 0 };
+    const int32_t data2[MAX] = { 3, 4, 5, 6, 2, 1, 0, 0, 0, 0 };
+    const int32_t data3[MAX] = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+
+    generator(data1, 3);
+    generator(data2, 6);
+    generator(data3, 9);
+
+
+    std::cout << "Before    Sorting: "; readFile();
+                                        sortFile();
+    std::cout << "After     Sorting: "; readFile();
+
+
+    return 0;
+}
+
+
+
+void generator(const int32_t array[MAX], unsigned int size) {
+    std::ofstream stream(FILE_PATH, std::ios::out | std::ios::binary);
+
+    if (stream.is_open() == false) {
+        std::cerr << ERROR_FILE_O << std::endl;
+
+        return;
+    }
+
+    stream.write(reinterpret_cast<const char*>(array), size * sizeof(int32_t));
+
+    if (stream.fail()) {
+        std::cerr << ERROR_FILE_W << std::endl;
+
+        return;
+    }
+
+    stream.close();
+
+    if (stream.fail()) {
+        std::cerr << ERROR_FILE_W << std::endl;
+
+        return;
+    }
+}
+
+
+
+void readFile() {
+    std::ifstream stream(FILE_PATH, std::ios::in | std::ios::binary);
+
+    if (stream.is_open() == false) {
+        std::cerr << ERROR_FILE_O << std::endl;
+
+        return;
+    }
+
+    stream.seekg(0, std::ios::end);
+
+    std::streamsize fileSize = stream.tellg();
+
+    if (fileSize == 0) {
+        std::cerr << ERROR_FILE_O << ' ' << ERROR_STATUS_E << std::endl;
+
+        return;
+    }
+
+    if (fileSize % sizeof(int32_t) != 0) {
+        std::cerr << ERROR_FILE_O << ' ' << ERROR_STATUS_C << std::endl;
+
+        return;
+    }
+
+    unsigned int size = fileSize / sizeof(int32_t);
+
+    stream.seekg(0, std::ios::beg);
+
+    for (unsigned int i = 0; i < size; ++i) {
+        int32_t currentNumber = 0;
+
+        stream.read(reinterpret_cast<char*>(&currentNumber), sizeof(int32_t));
+
+        if (stream.fail() || stream.gcount() != sizeof(int32_t)) {
+            std::cerr << ERROR_FILE_R << std::endl;
+
+            stream.close(); return;
+        }
+
+        std::cout << currentNumber << ' ';
+    }
+
+    std::cout << std::endl;
+
+    stream.close();
+}
+
+void sortFile() {
+    std::fstream stream(FILE_PATH, std::ios::in | std::ios::out | std::ios::binary);
+
+    if (stream.is_open() == false) {
+        std::cerr << ERROR_FILE_O << std::endl;
+
+        return;
+    }
+
+    stream.seekg(0, std::ios::end);
+
+    std::streampos fileSize = stream.tellg();
+
+    if (fileSize == 0) {
+        std::cerr << ERROR_FILE_D << ' ' << ERROR_STATUS_E << std::endl;
+
+        return;
+    }
+
+    if (fileSize % sizeof(int32_t) != 0) {
+        std::cerr << ERROR_FILE_D << ' ' << ERROR_STATUS_C << std::endl;
+
+        return;
+    }
+
+    unsigned int size = fileSize / sizeof(int32_t);
+
+    for (unsigned int i = 0; i < size - 1; ++i) {
+        for (unsigned int j = i + 1; j < size; ++j) {
+            int32_t value1 = 0;
+            int32_t value2 = 0;
+
+
+            stream.seekg(i * sizeof(int32_t), std::ios::beg);
+
+            stream.read(reinterpret_cast<char*>(&value1), sizeof(int32_t));
+
+            if (stream.fail() || stream.gcount() != sizeof(int32_t)) {
+                std::cerr << ERROR_FILE_R << std::endl;
+
+                stream.close(); return;
+            }
+
+            stream.seekg(j * sizeof(int32_t), std::ios::beg);
+
+            stream.read(reinterpret_cast<char*>(&value2), sizeof(int32_t));
+
+            if (stream.fail() || stream.gcount() != sizeof(int32_t)) {
+                std::cerr << ERROR_FILE_R << std::endl;
+
+                stream.close(); return;
+            }
+
+
+            if (value1 > value2) {
+                stream.seekp(i * sizeof(int32_t), std::ios::beg);
+
+                stream.write(reinterpret_cast<const char*>(&value2), sizeof(int32_t));
+
+                if (stream.fail()) {
+                    std::cerr << ERROR_FILE_W << std::endl;
+
+                    stream.close(); return;
+                }
+
+                stream.seekp(j * sizeof(int32_t), std::ios::beg);
+
+                stream.write(reinterpret_cast<const char*>(&value1), sizeof(int32_t));
+
+                if (stream.fail()) {
+                    std::cerr << ERROR_FILE_W << std::endl;
+
+                    stream.close(); return;
+                }
+            }
+        }
+    }
+
+    stream.close();
+
+    if (stream.fail()) {
+        std::cerr << ERROR_FILE_W << std::endl;
+
+        return;
+    }
+}
